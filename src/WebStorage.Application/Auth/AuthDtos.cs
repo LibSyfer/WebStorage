4f2ef067b2130fae14bbee6f6251fc9ff6ae0287
libsyfer <libsyfer@gmail.com>
2026-04-19 21:52:42 +0300

    feat: created auth serivices intrefaces

---
+++ src/WebStorage.Application/Auth/AuthDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStorage.Application.Auth;
+
+public sealed record RegisterRequest(string Email, string Password);
+public sealed record LoginRequest(string Email, string Password);
+
+public sealed record AuthResponse(string AccessToken, string Email, IReadOnlyList<string> Roles, DateTime ExpiresAtUtc);
+
+public sealed record AuthSessionResult(AuthResponse Auth, string RefreshToken);
+
---
+++ src/WebStorage.Application/Auth/IJwtTokenGenerator.cs
@@ -0,0 +1,6 @@
+﻿namespace WebStorage.Application.Auth;
+
+public interface IJwtTokenGenerator
+{
+    string CreateAccessToken(string userId, string email, IReadOnlyList<string> roles, DateTime expiresAtUtc);
+}
