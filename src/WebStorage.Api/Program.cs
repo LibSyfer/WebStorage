538b013aeffb1aab64afbf4dc437c678e78535a0
libsyfer <libsyfer@gmail.com>
2026-04-20 16:14:24 +0300

    feat: configured web application

--- src/WebStorage.Api/Program.cs
+++ src/WebStorage.Api/Program.cs
@@ -1,6 +1,49 @@
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using WebStorage.Api;
+using WebStorage.Api.Filters;
+using WebStorage.Application;
+using WebStorage.Application.Auth;
+using WebStorage.Infrastructure;
+using WebStorage.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<LogActionFilter>();
+});
+builder.Services.AddOpenApi();
+
+builder.ConfigureAuthentication();
+builder.ConfigureAuthorization();
+builder.ConfigureCors();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.MapOpenApi();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Public endpoint!");
+app.MapGet("/me", (ClaimsPrincipal user) =>
+{
+    return $"Protected endpoint! Hello, {user.Identity?.Name}!";
+}).RequireAuthorization();
+
+app.MapGet("/admin", (ClaimsPrincipal user) =>
+{
+    return $"Admin endpoint! Hello, {user.Identity?.Name}!";
+}).RequireAuthorization(new AuthorizeAttribute { Roles = RoleNames.Admin });
+
+await DbSeeder.SeedAsync(app.Services, app.Configuration);
 
 app.Run();
