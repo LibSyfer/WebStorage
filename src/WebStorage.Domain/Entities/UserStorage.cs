9a73b6188b5e208c1294ddaf1f90db4dcf42f8e7
libsyfer <libsyfer@gmail.com>
2026-04-21 00:09:22 +0300

    feat: created domain storage entities

---
+++ src/WebStorage.Domain/Entities/UserStorage.cs
@@ -0,0 +1,32 @@
+﻿using WebStorage.Domain.Common;
+
+namespace WebStorage.Domain.Entities;
+
+public class UserStorage : Entity
+{
+    public string UserId { get; set; } = string.Empty;
+    public long UsedBytes { get; set; }
+    public long MaxBytes { get; set; }
+
+    public bool TryReserve(long bytes)
+    {
+        if (UsedBytes + bytes > MaxBytes) return false;
+
+        UsedBytes += bytes;
+        return true;
+    }
+
+    public bool TryRelease(long bytes)
+    {
+        if (UsedBytes - bytes < 0) return false;
+
+        UsedBytes -= bytes;
+        return true;
+    }
+
+    public void ChangeQuota(long newQuota)
+    {
+        MaxBytes = newQuota;
+        if (MaxBytes < 0) MaxBytes = 0;
+    }
+}
