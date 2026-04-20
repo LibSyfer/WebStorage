1dde402be7cb03c7a4dc9dbeb44d58d89cb26db9
libsyfer <libsyfer@gmail.com>
2026-04-21 00:27:51 +0300

    chore: created migrations

--- src/WebStorage.Infrastructure/Migrations/20260420212722_AddedStorageEntities.Designer.cs
+++ src/WebStorage.Infrastructure/Migrations/20260420212722_AddedStorageEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebStorage.Infrastructure.Data;
@@ -11,9 +12,11 @@ using WebStorage.Infrastructure.Data;
 namespace WebStorage.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260420212722_AddedStorageEntities")]
+    partial class AddedStorageEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
